7dd7f3403acc71b529de280479230084c0cf7be8
riidhopangestu <raidou1998@gmail.com>
2022-09-02 18:04:27 +0700

    update 2 september

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { AppService, Products } from '../app.service';
 import { FormGroup, FormControl } from '@angular/forms';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort, Sort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
 export interface PeriodicElement {
   name: string;
   position: number;
@@ -10,18 +11,7 @@ export interface PeriodicElement {
   symbol: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+const ELEMENT_DATA: Products[] = [];
 
 @Component({
   selector: 'app-dashboard',
@@ -32,16 +22,24 @@ export class DashboardComponent implements OnInit {
   displayedColumns1: string[] = ['id', 'title', 'description', 'price'];
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
-
   data: Products[] = [];
 
   dataApp : any
+  posts : any
+
+  searchField: string = '';
+  clearSearchField() {
+    this.searchField = '';
+  }
+
   range = new FormGroup({
     start: new FormControl<Date | null>(null),
     end: new FormControl<Date | null>(null),
   });
+  
 
-  @ViewChild('empTbSort') empTbSort = new MatSort();
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
@@ -52,13 +50,17 @@ export class DashboardComponent implements OnInit {
     private appService : AppService
   ) { 
     this.appService.getDataProduct().subscribe((response:any) => {
-      this.data = response.products
-      console.log(this.data)
+      this.dataSource = response.products
+      console.log(this.dataSource)
+      this.posts = this.dataSource
+
+      this.dataSource = new MatTableDataSource(this.posts);
+      this.dataSource.paginator = this.paginator
+      this.dataSource.sort = this.sort
     })
   }
 
-  ngAfterViewInit() {    
-    this.dataSource.sort = this.empTbSort;
+  ngAfterViewInit() {
 }
 
   ngOnInit(): void {
